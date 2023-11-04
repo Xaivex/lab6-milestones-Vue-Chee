8cbf3989cf338414f00214abab2afbd8a6ea3cca
Xaivex <viethoj@gmail.com>
2023-11-04 13:19:35 -0500

    milestone 1

--- app/src/main/java/com/cs407/lab6_milestones/MainActivity.java
+++ app/src/main/java/com/cs407/lab6_milestones/MainActivity.java
@@ -1,14 +1,33 @@
 package com.cs407.lab6_milestones;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity {
+
+    private final LatLng mDestinationLatlng = new LatLng( 43.075342,  -89.404078);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatlng)
+                    .title("Destination"));
+        });
+
     }
+
 }
