4329624814eb69b8815dce5909b925e4e2bce531
cthao17 <rororm17@gmail.com>
2023-11-04 16:31:42 -0500

    Milestone 2

--- app/src/main/java/com/cs407/lab6_milestones/MainActivity.java
+++ app/src/main/java/com/cs407/lab6_milestones/MainActivity.java
@@ -1,33 +1,84 @@
 package com.cs407.lab6_milestones;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends FragmentActivity {
-
     private final LatLng mDestinationLatlng = new LatLng( 43.075342,  -89.404078);
     private GoogleMap mMap;
-
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
         mapFragment.getMapAsync(googleMap -> {
             mMap = googleMap;
 
             googleMap.addMarker(new MarkerOptions()
-                    .position(mDestinationLatlng)
-                    .title("Destination"));
+                    .position(mDestinationLatlng).title("Destination"));
+            displayMyLocation();
         });
-
     }
 
+    private void displayMyLocation() {
+        // Check if permission granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        // If not, ask for it
+        if (permission==PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        // If permission granted, display marker at current location
+        else {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                            if (task.isSuccessful() && mLastKnownLocation != null) {
+                                mMap.addPolyline(new PolylineOptions().add(
+                                        new LatLng(mLastKnownLocation.getLatitude(),
+                                                mLastKnownLocation.getLongitude()), mDestinationLatlng));
+                                mMap.addMarker(new MarkerOptions()
+                                        .position(new LatLng(mLastKnownLocation.getLatitude(),
+                                                mLastKnownLocation.getLongitude()))
+                                        .title("Current"));
+                            }
+                    });
+        }
+    }
+
+//    @SuppressLint("MissingSuperCall")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
+            }
+        }
+    }
 }
